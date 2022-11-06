d34cf90a0a9d214ce6c047b9abcc0748af726a8b
prithvianilk <thepirateking3d2y@gmail.com>
2022-11-06 15:42:32 +0530

    Add basic setup

--- cmd/titanic/titanic.go
+++ cmd/titanic/titanic.go
@@ -2,9 +2,106 @@ package main
 
 import (
 	"net/http"
+	"net/rpc"
+	"time"
+
+	"sync"
 
 	"github.com/gin-gonic/gin"
 )
+
+type ServerState int
+
+const (
+	follower ServerState = iota
+	leader
+	candidate
+)
+
+const electionStartDuration = time.Millisecond * 150
+
+type RaftApp struct {
+	state         ServerState
+	mu            sync.Mutex
+	term          int
+	lastResetTime time.Time
+	clients       []rpc.Client
+}
+
+type RequestVoteArgs struct {
+	term int
+}
+
+type RequestVoteReply struct {
+	term        int
+	voteGranted bool
+}
+
+func (app *RaftApp) startElectionTimer() {
+	app.mu.Lock()
+	app.lastResetTime = time.Now()
+	currentTerm := app.term
+	app.mu.Unlock()
+	resetCheckTime := time.Millisecond * 10
+	ticker := time.NewTicker(resetCheckTime)
+	for {
+		<-ticker.C
+		app.mu.Lock()
+
+		if app.state == leader {
+			app.mu.Unlock()
+			return
+		}
+
+		hasTermChanged := app.term != currentTerm
+		if hasTermChanged {
+			app.mu.Unlock()
+			return
+		}
+
+		timeSinceLastReset := time.Since(app.lastResetTime)
+		shouldStartElection := timeSinceLastReset >= electionStartDuration
+		if shouldStartElection {
+			app.startNewElection()
+			app.mu.Unlock()
+			return
+		}
+	}
+}
+
+func (app *RaftApp) startNewElection() {
+	app.state = candidate
+	app.term++
+
+	replicaCount := len(app.clients) + 1
+	votesReceived := 1
+	for _, client := range app.clients {
+		go func(client rpc.Client) {
+			args := RequestVoteArgs{app.term}
+			var reply RequestVoteReply
+			err := client.Call("RequestVote", args, &reply)
+			if err != nil {
+				return
+			} else if app.state != candidate {
+				return
+			} else if reply.term > app.term {
+				app.state = follower
+				app.term = reply.term
+				return
+			} else if !reply.voteGranted {
+				return
+			}
+			votesReceived++
+			shouldMakeLeader := votesReceived*2 >= replicaCount
+			if shouldMakeLeader {
+				app.makeLeader()
+				return
+			}
+		}(client)
+	}
+
+	go app.startElectionTimer()
+}
 
 func main() {
 	const addr = ":3530"
